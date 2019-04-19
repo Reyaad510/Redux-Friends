cf0c8460edccaa549c294cbe45965d74890c2cea
Reyaad Rafeek <reyaad510@gmail.com>
2019-04-18 20:52:07 -0400

    Created login action creator

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { login } from '../actions';
 
 
 class Login extends React.Component {
@@ -10,26 +11,50 @@ class Login extends React.Component {
         }
     };
 
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    handleLogin = e => {
+        e.preventDefault();
+        this.props
+          .login(this.state.credentials)
+          .then(() => this.props.history.push('/protected'));
+    }
+
     render() {
         return (
 
             <div>
-                <form>
-                    <input 
-                    type='text'
-                    name='username'
+              <form onSubmit={this.handleLogin}>
+                <input
+                   type='text'
+                   name='username'
+                   value={this.state.credentials.username}
+                   onChange={this.handleChange}
                     />
-                    <input 
+                <input
                     type='password'
                     name='password'
-                    />
-                    <button> Log In</button>
-                </form>
-            </div>
+                    value={this.state.credentials.password}
+                    onChange={this.handleChange}
+                />
+                <button> Log In</button>
+            </form>
+        </div>
         );
     }
 }
 
+
+
 export default connect(
-    null, {}
+    null, { login }
 )(Login)
+
+// Lambda School i<3Lambd4
